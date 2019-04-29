e1ce1885cfe09a5d1cd86baa5dee4580f90ba354
141542153-ZhengXin <945302733@qq.com>
2019-04-29 16:58:26 +0800

    更新代码

--- src/main/java/com/spring/web/controller/WebController.java
+++ src/main/java/com/spring/web/controller/WebController.java
@@ -3,10 +3,13 @@ package com.spring.web.controller;
 import com.spring.web.model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
+import java.io.Writer;
 import java.util.Map;
 
 @Controller
@@ -29,7 +32,7 @@ public class WebController {
 
     @GetMapping("/getMappingExample")
     @ResponseBody
-    public void getMappingExample(Model model) {
+    public void getMappingExample() {
         logger.info("run getMappingExample");
     }
 
@@ -41,35 +44,97 @@ public class WebController {
 
     @GetMapping("/pathExample/{pathName}")
     @ResponseBody
-    public void pathExample(@PathVariable String pathName, Model model) {
-        logger.info("run postMappingExample - pathName:" + pathName);
+    public void pathExample(@PathVariable String pathName) {
+        logger.info("run pathExample - pathName:" + pathName);
     }
 
-    @GetMapping("/user/example6/{pathName}/{pathName2}")
-    public String example6(@PathVariable Map<String, String> pathMap, Model model) {
-        model.addAttribute("pathName", pathMap.get("pathName"));
-        model.addAttribute("pathName2", pathMap.get("pathName2"));
-        return "user";
+    @GetMapping("/pathExample/{pathName}/{pathName2}")
+    @ResponseBody
+    public void pathExample(@PathVariable Map<String, String> pathMap) {
+        logger.info("run pathExample - pathName:" + pathMap.get("pathName") + " - pathName2:" + pathMap.get("pathName"));
     }
 
-    @GetMapping("/user/example7/{pathName}")
-    public String example7(@PathVariable String pathName, @MatrixVariable int q, @MatrixVariable int r, Model model) {
-        model.addAttribute("pathName", pathName);
-        model.addAttribute("q", q);
-        model.addAttribute("r", r);
-        return "user";
+    @GetMapping("/pathAndMatrixExample/{pathName}")
+    @ResponseBody
+    public void pathAndMatrixExample(@PathVariable String pathName, @MatrixVariable int q, @MatrixVariable int r) {
+        logger.info("run pathAndMatrixExample - pathName:" + pathName + " - q:" + q + " - r:" + r);
     }
 
-    @GetMapping("/user/example8/{pathName}/{pathName2}")
-    public String example8(@MatrixVariable(name = "q", pathVar = "pathName") int q1, @MatrixVariable(name = "q", pathVar = "pathName2") int q2, Model model) {
-        model.addAttribute("q1", q1);
-        model.addAttribute("q2", q2);
-        return "user";
+    @GetMapping("/pathAndMatrixExample/{pathName}/{pathName2}")
+    @ResponseBody
+    public void pathAndMatrixExample(@MatrixVariable(name = "q", pathVar = "pathName") int q1, @MatrixVariable(name = "q", pathVar = "pathName2") int q2) {
+        logger.info("run pathAndMatrixExample - q1:" + q1 + " - q2:" + q2);
     }
 
-    @GetMapping("/user/example9/{pathName}")
-    public String example9(@MatrixVariable(required = false, defaultValue = "1") int q, Model model) {
-        model.addAttribute("q", q);
-        return "user";
+    @GetMapping("/pathAndMatrixExampleWithoutReq/{pathName}")
+    @ResponseBody
+    public void pathAndMatrixExample(@MatrixVariable(required = false, defaultValue = "1") int q) {
+        logger.info("run pathAndMatrixExample - q:" + q);
+    }
+
+    @GetMapping("/pathAndMatrixMapExample/{pathName}/{pathName2}")
+    @ResponseBody
+    public void pathAndMatrixMapExample(@MatrixVariable MultiValueMap<String, String> matrixVars, @MatrixVariable(pathVar = "pathName2") MultiValueMap<String, String> matrixVars2) {
+        logger.info("run pathAndMatrixMapExample - matrixVars:" + matrixVars + " - matrixVars2" + matrixVars2);
+    }
+
+    @GetMapping(value = "/consumesExample", consumes = "application/json")
+    @ResponseBody
+    public void consumesExample() {
+        logger.info("run consumesExample");
+    }
+
+    @GetMapping(value = "/consumesReverseExample", consumes = "!application/json")
+    @ResponseBody
+    public void consumesReverseExample() {
+        logger.info("run consumesReverseExample");
+    }
+
+    @GetMapping(value = "/producesExample", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @ResponseBody
+    public void producesExample() {
+        logger.info("run producesExample");
+    }
+
+    @GetMapping(value = "/paramsExample", params = "myParams")
+    @ResponseBody
+    public void paramsExample() {
+        logger.info("run paramsExample");
+    }
+
+    @GetMapping(value = "/paramsReverseExample", params = "!myParams")
+    @ResponseBody
+    public void paramsReverseExample() {
+        logger.info("run paramsReverseExample");
+    }
+
+    @GetMapping(value = "/paramsExampleByValue", params = "myParams=123")
+    @ResponseBody
+    public void paramsExampleByValue() {
+        logger.info("run paramsExampleByValue");
+    }
+
+    @GetMapping(value = "/headersExample", headers = "myHeader=myValue")
+    @ResponseBody
+    public void headersExample() {
+        logger.info("run headersExample");
+    }
+
+    @GetMapping(value = "/paramExample")
+    @ResponseBody
+    public void paramExample(@RequestParam("id") String id) {
+        logger.info("run paramExample - id:" + id);
+    }
+
+    @GetMapping(value = "/paramMapExample")
+    @ResponseBody
+    public void paramMapExample(@RequestParam MultiValueMap<String, String> map) {
+        logger.info("run paramExample - map:" + map);
+    }
+
+    @PostMapping(value = "/requestBodyExample")
+    @ResponseBody
+    public void requestBodyExample(@RequestBody(required = false) String id) {
+        logger.info("run requestBodyExample - id:" + id);
     }
 }
--- src/main/webapp/WEB-INF/jsp/user.jsp
+++ src/main/webapp/WEB-INF/jsp/user.jsp
@@ -12,52 +12,6 @@
     <title>SpringWeb</title>
 </head>
 <body>
-    <h1>example1</h1>
-    <li>id:${empty id ? user.id : id}</li>
-    <li>name:${empty name ? user.name : name}</li>
-    <li>age:${empty age ? user.age : age}</li>
-    <hr/>
 
-    <h1>example2</h1>
-    <li>id:${empty id ? user.id : id}</li>
-    <li>name:${empty name ? user.name : name}</li>
-    <li>age:${empty age ? user.age : age}</li>
-    <hr/>
-
-    <h1>example3</h1>
-    <li>id:${empty id ? user.id : id}</li>
-    <li>name:${empty name ? user.name : name}</li>
-    <li>age:${empty age ? user.age : age}</li>
-    <hr/>
-
-    <h1>example4</h1>
-    <li>id:${empty id ? user.id : id}</li>
-    <li>name:${empty name ? user.name : name}</li>
-    <li>age:${empty age ? user.age : age}</li>
-    <hr/>
-
-    <h1>example5</h1>
-    <li>pathName:${empty pathName ? pathName : pathName}</li>
-    <hr/>
-
-    <h1>example6</h1>
-    <li>pathName:${empty pathName ? pathName : pathName}</li>
-    <li>pathName2:${empty pathName2 ? pathName2 : pathName2}</li>
-    <hr/>
-
-    <h1>example7</h1>
-    <li>pathName:${empty pathName ? pathName : pathName}</li>
-    <li>q:${empty q ? q : q}</li>
-    <li>r:${empty r ? r : r}</li>
-    <hr/>
-
-    <h1>example8</h1>
-    <li>q1:${empty q1 ? q1 : q1}</li>
-    <li>q2:${empty q2 ? q2 : q2}</li>
-    <hr/>
-
-    <h1>example9</h1>
-    <li>q:${empty q ? q : q}</li>
-    <hr/>
 </body>
 </html>
