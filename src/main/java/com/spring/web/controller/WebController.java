cb22b255a8ce8ab6ad84370d636a544405b12177
141542153-ZhengXin <945302733@qq.com>
2019-05-09 13:59:26 +0800

    更新代码

--- src/main/java/com/spring/web/controller/WebController.java
+++ src/main/java/com/spring/web/controller/WebController.java
@@ -6,7 +6,6 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.Map;
 
 @Controller
